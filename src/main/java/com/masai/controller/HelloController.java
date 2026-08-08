07559221f7f8bc6c814ee5686e99df21c024a278
HuuTho021205 <trandinhhuutho@gmail.com>
2026-08-08 18:06:37 +0700

    test cicd

---
+++ src/main/java/com/masai/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.masai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("api/public/test")
+    public String TestCICD(){
+        return "hello cicd";
+    }
+}
